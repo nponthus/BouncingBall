e8bbd82cd0ee34570bc42e873dc1a7360fad5295
nponthus <nicolas.ponthus@gmx.com>
2015-06-12 09:37:56 +0200

    randomly excited 3 dof model
    three_ballsrnd: solution calculation
    accelerationrnd: acceleration with random profil
    use profilrand to generate H2 and H3

---
+++ accelerationrnd.m
@@ -0,0 +1,18 @@
+function [ A , Ek] = acceleration(z_ballsc, rho_balls, theta_balls,Hc)
+%Returns acceleration and elastic potential Energy
+global m g K I J
+z_balls=reshape(z_ballsc,1,length(z_ballsc));
+H=reshape(Hc,1,length(z_ballsc));
+    ind=find(z_balls-H <=0);
+    if isempty(ind)==false
+        A(1)=-g+sum(K(ind).*(H(ind)-z_balls(ind)).^(3/2))/m;
+        A(2)=sum(K(ind).*(H(ind)-z_balls(ind)).^(3/2).*rho_balls(ind).*sin(theta_balls(ind)))/I;
+        A(3)=-sum(K(ind).*(H(ind)-z_balls(ind)).^(3/2).*rho_balls(ind).*cos(theta_balls(ind)))/J;
+        Ek=sum(2/5*K(ind).*(H(ind)-z_balls(ind)).^(5/2));
+    else
+        A(1)=-g;
+        A(2)=0;
+        A(3)=0;
+        Ek=0;
+    end
+end
